91e2e213979a248066f604be5cc5f6e9ff38365c
Jake Bown <jakebown@gmail.com>
2015-09-04 14:44:39 +0100

    Cleanup JS file
    Made easier to add social networks to your social links.

--- share-anchor/js/share-anchor.js
+++ share-anchor/js/share-anchor.js
@@ -2,37 +2,44 @@
  * Anchor CMS Plugin
  * Plugin: Share Anchor
  * Author: Jake Bown
- * Update: 2 Dec 2014
+ * Update: 04 Sept 15
  */
+ 	var footer = $('.footnote');
+ 	var shareAnchor = {
+ 		
+ 		init:function() {
+ 			var that = this;
+	 		var fb = 'https://www.facebook.com/sharer/sharer.php?u=' + that.returnUrl(); //Facebook
+			var tw = 'https://twitter.com/intent/tweet?url='+ that.returnUrl() + '&text=' + that.returnTitle(); //Twitter
+			var gg = 'https://plus.google.com/share?url='+ that.returnUrl(); //Google+
+			var li = 'https://www.linkedin.com/shareArticle?mini=true&url='+ that.returnUrl()+'&title='+ that.returnTitle(); //LinkedIn
+			var tu = 'http://www.tumblr.com/share/link?url='+ that.returnUrl()+'&name='+ that.returnTitle(); //Tumblr
+ 			
+ 			var html = '<span id="share-anchor"><a href="'+fb+'" target="_blank" class="share-anchor-button share-anchor-facebook default"><i class="fa fa-facebook"></i></a> <a href="'+tw+'" target="_blank" class="share-anchor-button share-anchor-twitter default"><i class="fa fa-twitter"></i></a> <a href="'+gg+'" target="_blank" class="share-anchor-button share-anchor-google default"><i class="fa fa-google-plus"></i></a> <a href="'+li+'" target="_blank" class="share-anchor-button share-anchor-linkedin default"><i class="fa fa-linkedin"></i></a> <a href="'+tu+'" target="_blank" class="share-anchor-button share-anchor-tumblr default"><i class="fa fa-tumblr"></i></a></span>';
+			that.setup(html);
+ 		},
+ 		
+ 		returnTitle:function() {
+			var t = $(document).attr('title');
+			return encode(t);
+		}
+	
+		returnUrl:function(en) {
+			var h = $(location).attr('href');
+			return encode(h);
+		}
+	
+		encode:function(url) {
+			return encodeURIComponent(url);
+		}
+ 		
+ 		setup:function(html) {
+ 			footer.append(html);	
+ 		}
+ 	};
 
 	$(document).ready(function() {
-		$(".footnote").append(shareAnchor());
+		shareAnchor.init();
 	});
 
-	function shareAnchor() {
-		var fb = 'https://www.facebook.com/sharer/sharer.php?u=' + returnUrl(); //Facebook
-		var tw = 'https://twitter.com/intent/tweet?url='+returnUrl()+'&text='+returnTitle(); //Twitter
-		var gg = 'https://plus.google.com/share?url='+returnUrl(); //Google+
-		var li = 'https://www.linkedin.com/shareArticle?mini=true&url='+returnUrl()+'&title='+returnTitle(); //LinkedIn
-		var tu = 'http://www.tumblr.com/share/link?url='+returnUrl()+'&name='+returnTitle(); //Tumblr
 
-		var html = '<span id="share-anchor"><a href="'+fb+'" target="_blank" class="share-anchor-button share-anchor-facebook default"><i class="fa fa-facebook"></i></a> <a href="'+tw+'" target="_blank" class="share-anchor-button share-anchor-twitter default"><i class="fa fa-twitter"></i></a> <a href="'+gg+'" target="_blank" class="share-anchor-button share-anchor-google default"><i class="fa fa-google-plus"></i></a> <a href="'+li+'" target="_blank" class="share-anchor-button share-anchor-linkedin default"><i class="fa fa-linkedin"></i></a> <a href="'+tu+'" target="_blank" class="share-anchor-button share-anchor-tumblr default"><i class="fa fa-tumblr"></i></a></span>';
-		return html;
-
-
-	}
-
-
-	function returnTitle() {
-		var t = $(document).attr('title');
-		return encode(t);
-	}
-
-	function returnUrl(en) {
-		var h = $(location).attr('href');
-		return encode(h);
-	}
-
-	function encode(url) {
-		return encodeURIComponent(url);
-	}
